2cf6726edda62422ac081baf8740d7780952b289
snz-lan <shahnaz.abdulhameed2309@gmail.com>
2025-03-27 20:00:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MDP_assess',  # Package name
-    version='0.1',
-    packages=find_packages(),  # Automatically find packages in the project
-    install_requires=[  # External dependencies (if any)
-        # 'numpy', 'pandas'  # Example, if your project needs these
+    name='MDP_assess',
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",  # Add any dependencies here
     ],
+    author="Shahnaz Abdul Hameed",
+    author_email="shahnaz.abdulhameed2309@gmail.com",
+    description="A Markov Decision Process solver using Value Iteration",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/snz-lan/MDP_assess",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.7",
 )
